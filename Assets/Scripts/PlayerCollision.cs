86f01a00136ea0768d316bdf7cd304ec3d78648b
Mohamed-samy2 <mohamedsamyy02@gmail.com>
2025-05-15 18:12:07 +0300

    adding get hurt by mace and blinking animation

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,13 +1,32 @@
 using UnityEngine;
-
+using System.Collections;
 public class PlayerCollision : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag.Equals("Enemy"))
         {
+
+            HealthManager.health--;
+            if (HealthManager.health <= 0)
+            {
             PlayerManager.isGameOver = true;
             gameObject.SetActive(false);
+            }
+            else
+            {
+                StartCoroutine(GetHurt());
+            }
         }
+    }
+
+    IEnumerator GetHurt()
+    {
+        Physics2D.IgnoreLayerCollision(3, 7);
+        GetComponent<Animator>().SetLayerWeight(1, 1);
+        yield return new WaitForSeconds(2);
+        GetComponent<Animator>().SetLayerWeight(1, 0);
+        Physics2D.IgnoreLayerCollision(3, 7,false);
+
     }
 }
